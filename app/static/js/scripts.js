e67071fdfeefde0ed46cbc02f5466d652043bd93
Kyalo460 <kyalo460@gmail.com>
2024-10-21 16:15:48 +0300

    added hamburger

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,4 +1,12 @@
-$(document).ready(function() {
-    console.log("Document ready!");
-    // Additional JS functionalities can go here
+// $(document).ready(function() {
+//     console.log("Document ready!");
+//     // Additional JS functionalities can go here
+// });
+
+// A function to toggle the open on hambuger and nav
+const hamburger = document.getElementById('hamburger');
+const navbar = document.getElementById('navbar');
+
+hamburger.addEventListener('click', () => {
+    navbar.classList.toggle('open');
 });
