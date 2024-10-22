2f77aee5ba451eaf20ec4aaa1c3f1f9923fb436b
Kyalo460 <kyalo460@gmail.com>
2024-10-22 19:03:33 +0300

    worked on the dark mode

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -10,3 +10,32 @@ const navbar = document.getElementById('navbar');
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle('open');
 });
+
+// A function to toggle light and darkmode
+const darkmodeToggle = document.getElementById('darkmodeToggle');
+const body = document.getElementById('body');
+const listGroupItem = document.getElementById('list-group-item');
+
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme === 'dark') {
+    toggleMode();
+}
+
+function toggleMode() {
+    body.classList.toggle('dark-mode');
+    darkmodeToggle.classList.toggle('dark-mode');
+    hamburger.classList.toggle('dark-mode');
+    navbar.classList.toggle('dark-mode');
+
+    if (listGroupItem !== null) {
+        listGroupItem.classList.toggle('dark-mode');
+    }
+}
+
+darkmodeToggle.addEventListener('click', () => {
+    toggleMode();
+    // Save user preference in localStorage
+    const theme = darkmodeToggle.classList.contains('dark-mode') ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
+})
